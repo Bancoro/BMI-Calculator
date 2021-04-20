0350604f687cbcf2eaacddcc7ccac1a5ccc86adb
Bancoro <aaron.bancoro090010@my.lsgh.edu.ph>
2021-04-20 09:02:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-cybv9x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-cybv9x)
---
+++ pages/home/home.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  height: number;
+  weight: number;
+  bmi: number;
+  message: string;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  calculateBMI () {
+
+    this.bmi = this.weight/(this.height * this.height)
+
+    this.bmi = parseFloat(this.bmi.toFixed(2))
+
+    if (this.bmi < 18.5){
+      this.message = "which is underweight";
+    } else if (this.bmi >= 18.5 && this.bmi < 24.5){
+      this.message = "which is normal :)";
+    } else if (this.bmi >= 24.5 && this.bmi < 30){
+      this.message = "which is overweight";
+    } else {
+      this.message = "which is obese";
+    }
+  }
+
+}
